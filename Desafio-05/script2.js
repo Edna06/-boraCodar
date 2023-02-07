6a6374d146b3d620208be7ccc685d193eba2c955
Edna <edna.maria887@gmail.com>
2023-02-06 23:49:09 -0300

    update space

--- Desafio-05/script2.js
+++ Desafio-05/script2.js
@@ -1,114 +1,114 @@
 // capturando os botões
-const calculation = document.querySelector('.calculator .calculation')
-const display = document.querySelector('.result_display p')
-const number = document.querySelectorAll('[id*=key]')
-const operators = document.querySelectorAll('[id*=Operator]')
+const calculation = document.querySelector('.calculator .calculation');
+const display = document.querySelector('.result_display p');
+const number = document.querySelectorAll('[id*=key]');
+const operators = document.querySelectorAll('[id*=Operator]');
 const equalSymbolDisplay = document.querySelector(
   '.number_display .result_display .equal_display'
-)
+);
 
-let newNumber = true //no inicío deve ser um novo número
-let operator //vai guardar o meu operador
-let previousNumber //vai guardar o meu número
+let newNumber = true;
+let operator;
+let previousNumber;
 
-const operationPendant = () => operator !== undefined
+const operationPendant = () => operator !== undefined;
 
 const calculate = () => {
   if (operationPendant()) {
     //se existir essa operação pendente, aí ele vai calcular
-    const createCurrent = parseFloat(display.textContent.replace(',', '.')) //transformando em número
-    newNumber = true
+    const createCurrent = parseFloat(display.textContent.replace(',', '.')); //transformando em número
+    newNumber = true;
 
     if (operator == '%') {
-      updateDisplay((createCurrent / 100) * previousNumber)
-      calculation.innerHTML = previousNumber + operator + createCurrent
+      updateDisplay((createCurrent / 100) * previousNumber);
+      calculation.innerHTML = previousNumber + operator + createCurrent;
     } else {
-      const result = eval(`${previousNumber} ${operator} ${createCurrent} `)
-      calculation.innerHTML = previousNumber + operator + createCurrent
-      updateDisplay(result)
+      const result = eval(`${previousNumber} ${operator} ${createCurrent} `);
+      calculation.innerHTML = previousNumber + operator + createCurrent;
+      updateDisplay(result);
     }
   }
-}
+};
 
 const updateDisplay = text => {
   //se for um novo número ele não vai concatenar, vai adicionar um novo número apenas
   if (newNumber) {
-    display.textContent = text.toLocaleString('pt-BR') //vai trazer o símbolo usado no decimal no Brasil
-    newNumber = false //quando eu clicar em um outro número não será mais 'um novo', então irá concatenar
+    display.textContent = text.toLocaleString('pt-BR'); //vai trazer o símbolo usado no decimal no Brasil
+    newNumber = false; //quando eu clicar em um outro número não será mais 'um novo', então irá concatenar
   } else {
-    display.textContent += text.toLocaleString('pt-BR') //concatenando os números com os que já tem
+    display.textContent += text.toLocaleString('pt-BR'); //concatenando os números com os que já tem
   }
 }
 
-const insertNumber = event => updateDisplay(event.target.textContent) //para trazer para mim o alvo que cliquei
+const insertNumber = event => updateDisplay(event.target.textContent); //para trazer para mim o alvo que cliquei
 
 //executa uma função para cada elemento de um array
-number.forEach(number => number.addEventListener('click', insertNumber))
+number.forEach(number => number.addEventListener('click', insertNumber));
 
 //selecionando o operador
 const selectOperator = event => {
   //fazendo validação
   //se não for um novo número
   if (!newNumber) {
-    calculate()
-    newNumber = true
-    operator = event.target.textContent //guardando na memória o operador e o número que apareceu antes do operador
-    previousNumber = parseFloat(display.textContent.replace(',', '.'))
+    calculate();
+    newNumber = true;
+    operator = event.target.textContent; //guardando na memória o operador e o número que apareceu antes do operador
+    previousNumber = parseFloat(display.textContent.replace(',', '.'));
   }
-}
+};
 
 operators.forEach(operator =>
-  operator.addEventListener('click', selectOperator)
-)
+  operator.addEventListener('click', selectOperator);
+);
 
 // config equal button
 document.querySelector('button.symbol_equal').addEventListener('click', () => {
-  calculate()
-  equalSymbolDisplay.classList.remove('equal_display')
-  operator = undefined
+  calculate();
+  equalSymbolDisplay.classList.remove('equal_display');
+  operator = undefined;
 })
 
 // config clear display
 document.querySelector('button#cancel_entry').addEventListener('click', () => {
-  display.textContent = display.textContent.slice(0, -1)
-})
+  display.textContent = display.textContent.slice(0, -1);
+});
 
 // config cancel entry
 document.querySelector('button#clear').addEventListener('click', () => {
-  display.textContent = ''
-  operator = undefined
-  previousNumber = undefined
-  createCurrent = true
-  equalSymbolDisplay.classList.add('equal_display')
-  calculation.innerHTML = ''
-})
+  display.textContent = '';
+  operator = undefined;
+  previousNumber = undefined;
+  createCurrent = true;
+  equalSymbolDisplay.classList.add('equal_display');
+  calculation.innerHTML = '';
+});
 
 // function invert signal
 const invert = document
   .querySelector('button.invert')
   .addEventListener('click', () => {
-    var myNumber = display.textContent
+    var myNumber = display.textContent;
 
-    display.textContent = myNumber *= -1
-  })
+    display.textContent = myNumber *= -1;
+  });
 
 // lembrando que a vírgula é um decimal
 
 // vai procurar de existe vírgula no meu texto
-const existDecimal = () => display.textContent.indexOf(',') !== -1 //se ele me trouxer um número diferente de -1, quer dizer que é true
-const existValue = () => display.textContent.length > 0 //ele vai avaliar o tamanho
+const existDecimal = () => display.textContent.indexOf(',') !== -1; //se ele me trouxer um número diferente de -1, quer dizer que é true
+const existValue = () => display.textContent.length > 0; //ele vai avaliar o tamanho
 
 const insertDecimal = () => {
   if (!existDecimal()) {
     if (existValue()) {
-      updateDisplay(',')
+      updateDisplay(',');
     } else {
-      updateDisplay('0,')
+      updateDisplay('0,');
     }
   }
 }
 
-document.querySelector('button#comma').addEventListener('click', insertDecimal)
+document.querySelector('button#comma').addEventListener('click', insertDecimal);
 
 // add opção de digitar pelo teclado
 const mapKey = {
@@ -132,16 +132,16 @@ const mapKey = {
   '+': 'plusOperator',
   ',': 'comma',
   i: 'invertSign'
-}
+};
 
 const mapKeyboard = event => {
-  var key = event.key
+  var key = event.key;
 
-  const keyPermit = () => Object.keys(mapKey).indexOf(key) !== -1 // o indexof me retorna -1 quando é false
+  const keyPermit = () => Object.keys(mapKey).indexOf(key) !== -1; // o indexof me retorna -1 quando é false
 
   if (keyPermit()) {
-    document.getElementById(mapKey[key]).click()
+    document.getElementById(mapKey[key]).click();
   }
 }
 
-document.addEventListener('keydown', mapKeyboard) //keydown vai substituir o clique con o mouse
+document.addEventListener('keydown', mapKeyboard); 
